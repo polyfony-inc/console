ab590c15eb5becc91c92a2f4eaf3d0b87e9959aa
Julien <nospam.github@gribouille.eu>
2018-02-21 23:33:54 +0100

    CRUD Generator templates draft

---
+++ templates/Views/Index.php
@@ -0,0 +1,32 @@
+<div class="container">
+	<div class="col-12">
+		<h1>
+			<span class="fa fa-__Icon__"></span>
+			__Table__
+		</h1>
+
+		<table class="table table-striped table-hover">
+
+			<thead>
+				<!-- Legend -->
+				<tr>
+
+				</tr>
+				<!-- Legend -->
+
+				<!-- Filters -->
+				<tr>
+
+				</tr>
+				<!-- Filters -->
+			</thead>
+			<tbody>
+				<?php foreach($this->__Plural__ as __Singular__): ?>
+
+
+				<?php endforeach; ?>
+			</tbody>
+
+		</table>
+	</div>
+</div>
